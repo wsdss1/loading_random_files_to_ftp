6b731fd7fcbebd2d8156125a72686c62bcc38c93
p <gf>
2019-07-08 19:20:10 +0300

    added file range

--- load_to_ftp_random_files.py
+++ load_to_ftp_random_files.py
@@ -9,16 +9,21 @@ check in DB messages about this files and write info to file result.
 
 import os, logging, logging.config, time, datetime, shutil, random, uuid, sys, pyodbc
 from ftplib import FTP
+from multiprocessing import Process
 
-HOST = "10.200.12.228"
+HOST = "192.168.10.13"
 PORT = 21
 UNAME = "ftpuser"
 PS = "12345"
-ftp = FTP('10.200.12.228', 'ftpuser', '12345')
-SQL_SERVER_CONNECT = "Driver={SQL Server Native Client 11.0};Server=10.200.12.223;Database=SSNTI;Trusted_Connection=no;uid=test;pwd=Password12!"
+ftp = FTP('192.168.10.13', 'ftpuser', '12345')
+SQL_SERVER_CONNECT = "Driver={SQL Server Native Client 11.0};Server=192.168.10.24;Database=SSNTI;Trusted_Connection=no;uid=test;pwd=Password12!"
+
+FTP_PATH = ['/home/ftpuser/','/home/ftpuser/test_ras','/home/ftpuser/test_ras_zip']
 
 waveform_archive_t = ['to','t01','t02','t03','t04','t05','t06','t07','t08','t09','t0A','t0B','t0C','t0D','t0E','t0F']
 waveform_archive_d = ['do','d01','d02','d03','d04','d05','d06','d07','d08','d09','d0A','d0B','d0C','d0D','d0E','d0F']
+waveform_archive_zip = ['zip']
+waveform_archive_os = ['os1.zip','os2.zip']
 
 temp_folder = "temp_folder"
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -54,29 +59,31 @@ def create_dir(created_directory: str):
 
 # create random file 
 def rnd_file_create():
-    archives = [waveform_archive_t, waveform_archive_d]
-    used_archive = random.choice(archives)
-    # generate a random UUID
-    filename = f'{uuid.uuid4()}'
-    create_dir(temp_folder)
-    for i in used_archive:
-        full_filename = f'{current_dir}/temp_folder/{filename}.{i}'
-        newfile = open(full_filename, 'wb')
-        size = random.randint(100000, 9999999) # in bytes
-        newfile.seek(size)
-        newfile.write(b'\0')
-        logger.info(f'{sys._getframe().f_code.co_name}  {full_filename}  size= {size} bytes')
-        newfile.close()
-    return filename
+    archives = [waveform_archive_t, waveform_archive_d, waveform_archive_zip, waveform_archive_os]
+    #used_archive = random.choice(archives)
+    for arr in archives:
+        # generate a random UUID
+        filename = f'{uuid.uuid4()}'
+        create_dir(temp_folder)
+        for i in arr:
+            full_filename = f'{current_dir}/temp_folder/{filename}.{i}'
+            newfile = open(full_filename, 'wb')
+            size = random.randint(100000, 9999999) # in bytes
+            newfile.seek(size)
+            newfile.write(b'\0')
+            logger.info(f'{sys._getframe().f_code.co_name}  {full_filename}  size= {size} bytes')
+            newfile.close()
+        return filename
+
 
 # upload file on ftp
-def upload_ftp(file: str):
+def upload_ftp(file: str, ftp_path: str):
     logger.info(f'{sys._getframe().f_code.co_name}  try connect to {HOST}:{PORT} ftp server')
     ftp.connect(HOST, PORT)
     logger.info(f'{sys._getframe().f_code.co_name}  try login to ftp server')
     ftp.login(UNAME, PS)
     # Перейти в директорию
-    ftp.cwd('/home/ftpuser/')
+    ftp.cwd(ftp_path)
     logger.info(f'{sys._getframe().f_code.co_name}  read file from temp_folder')
     myfile = open(f'{current_dir}/temp_folder/{file}', 'rb')
     logger.info(f'{sys._getframe().f_code.co_name}  try stor file to ftp server')
@@ -90,8 +97,14 @@ def read_local_dir():
     num_files = len(os.listdir(f'./{temp_folder}'))
     logger.info(f'{sys._getframe().f_code.co_name}  read {num_files} files in {temp_folder}')
     for file in file_list:
-        logger.info(f'{sys._getframe().f_code.co_name}  try upload {file} to ftp server') 
-        upload_ftp(file)
+        logger.info(f'{sys._getframe().f_code.co_name}  try upload {file} to ftp server')
+        ################################
+        if file.endswith('.zip'):
+            upload_ftp(file, FTP_PATH[2])
+        elif file.endswith('os1.zip','os2.zip'):
+            upload_ftp(file, FTP_PATH[1])
+        else:
+            upload_ftp(file, FTP_PATH[0])
         create_dir(NEW_PATH)
         logger.info(f'{sys._getframe().f_code.co_name}  send file {file} to {NEW_PATH}') 
         shutil.move(f'{current_dir}\\temp_folder\{file}', f'{current_dir}\{NEW_PATH}')
@@ -107,7 +120,7 @@ def sql_connect_select(file_name: str):
     rows_count = cursor.execute(query_sql)
     while cursor.rowcount == 0:
         logger.info(f'{sys._getframe().f_code.co_name}  wait information from SQL in table about this {file_name} file') 
-        print (f'{file_name} wait information from SQL in table about this {file_name} file \n')
+        print(f'{file_name} wait information from SQL in table about this {file_name} file \n')
         time.sleep(60)
         query_sql = f"SELECT * FROM [SSNTI_20181213].[dbo].[NTI_SYSEVENT] where Message like N'%{file_name}%' and Type='201'"
         rows_count = cursor.execute(query_sql)
@@ -116,12 +129,12 @@ def sql_connect_select(file_name: str):
 
 # create file result
 def file_result(message: str):
-    file = open(file_with_result, 'a'):
-    file.write(f'{message}')
+    file = open(file_with_result, 'a')
+    file.write(f'{message}\n')
     file.close()
 
 if __name__ == '__main__':
     created_file = rnd_file_create()
     read_local_dir()
     sql_connect_select(created_file)   
-    file_result(f'{created_file} {}')
+    file_result(f'file {created_file} create -> file on ftp -> info in DB\n')
